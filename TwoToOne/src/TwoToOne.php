5ff42042b1cd52902d51b0d009714fd594c4cc8c
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-03-17 13:46:39 +0100

    TwoToOne: implementation and test

--- TwoToOne/src/TwoToOne.php
+++ TwoToOne/src/TwoToOne.php
@@ -2,10 +2,23 @@
 
 namespace TwoToOne;
 
+/**
+ * Class TwoToOne
+ * @package TwoToOne
+ */
 class TwoToOne
 {
+    /**
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
     public static function twoToOne(string $first, string $second)
     {
-        return $first . $second;
+        $conectedStrings = $first . $second;
+        $result = array_unique(str_split($conectedStrings));
+        asort($result);
+
+        return implode('', $result);
     }
 }
--- TwoToOne/tests/TwoToOneTest.php
+++ TwoToOne/tests/TwoToOneTest.php
@@ -4,10 +4,26 @@ namespace Tests;
 
 use TwoToOne\TwoToOne;
 
+/**
+ * Class TwoToOneTest
+ * @package Tests
+ */
 class TwoToOneTest extends \PHPUnit\Framework\TestCase
 {
-    public function testTwoToOneFunction()
+    /**
+     * @param $actual
+     * @param $expected
+     */
+    private function revTest($actual, $expected)
     {
-        $this->assertEquals('abc', TwoToOne::twoToOne('a', 'bc'));
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testBasics()
+    {
+        $this->revTest(TwoToOne::twoToOne("aretheyhere", "yestheyarehere"), "aehrsty");
+        $this->revTest(TwoToOne::twoToOne("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu");
+        $this->revTest(TwoToOne::twoToOne("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy");
+        $this->revTest(TwoToOne::twoToOne("lordsofthefallen", "gamekult"), "adefghklmnorstu");
     }
 }
