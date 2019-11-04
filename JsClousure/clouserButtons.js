17cef6619de206d55e3615cc5709cdf10b0612dc
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-11-04 23:34:26 +0100

    Clouser Button proper fix

--- JsClousure/clouserButtons.js
+++ JsClousure/clouserButtons.js
@@ -1,5 +1,7 @@
 for (var i = 1; i <= 3; i++) {
-  document.getElementById("button-" + i).onclick = function() {
-    alert("Kliknięto w przycisk o numerze " + i);
-  };
+  document.getElementById("button-" + i).onclick = (function(increment) {
+    return function () {
+      alert("Kliknięto w przycisk o numerze " + increment);
+    }
+  })(i);
 }
